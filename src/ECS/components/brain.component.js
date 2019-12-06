3bcb264ac821fd45af6b252b6588fb7cbc102fd5
luetkemj <luetkemj@gmail.com>
2019-12-06 06:37:52 -0600

    brain

--- src/ECS/components/brain.component.js
+++ src/ECS/components/brain.component.js
@@ -1,12 +1,7 @@
 const componentBrain = (params = {}) => {
   return {
-    goalStack: [],
-    desires: {
-      bored: 0,
-      thirst: 0,
-      hunger: 0,
-      anger: 0
-    }
+    goalStack: ["attack", "eat", "drink", "hug"],
+    needs: []
   };
 };
 
